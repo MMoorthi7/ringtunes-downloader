1cbaeb1100213a757e93835ea307b1c6dffc1e79
moorthideveloper <msnkl6379@gmail.com>
2024-10-03 19:36:39 +0530

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/ringtunes-downloaderg/"
-})
+  base: "/ringtunes-downloader/", // Base path for production build
+
+  server: {
+    port: 3000, // Set the development server port
+  },
+
+  build: {
+    outDir: 'dist', // Directory for production build output
+  },
+});
